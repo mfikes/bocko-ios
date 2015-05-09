3c87971de32ea47226c886074504499b23579f92
Mike Fikes <mike@fikesfarm.com>
2015-05-09 13:29:44 -0400

    Add core async so that it is available

--- ClojureScript/project.clj
+++ ClojureScript/project.clj
@@ -2,6 +2,7 @@
   :description "Use Bocko on iOS"
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [org.clojure/clojurescript "0.0-3255"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [bocko "0.3.0-SNAPSHOT"]
                  [org.omcljs/ambly "0.1.0-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "1.0.5"]]
