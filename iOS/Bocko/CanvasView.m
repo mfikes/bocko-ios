dc51dac8e617184b41f6bf6e6bd2c170be60297d
Mike Fikes <mike@fikesfarm.com>
2015-05-09 12:54:18 -0400

    Stretch to fit screen

--- iOS/Bocko/CanvasView.m
+++ iOS/Bocko/CanvasView.m
@@ -16,12 +16,14 @@ static int pixB[40][40];
 -(void) drawRect:(CGRect)rect
 {
     [super drawRect:rect];
+    CGFloat pixelWidth = rect.size.width/40;
+    CGFloat pixelHeight = rect.size.height/40;
     CGContextRef context = UIGraphicsGetCurrentContext();
     for (int x = 0; x<40; x++) {
         for (int y = 0; y<40; y++) {
-            CGRect rectangle = CGRectMake(x*16, y*8, 16, 8);
+            CGRect rectangle = CGRectMake(x*pixelWidth, y*pixelHeight, pixelWidth+1, pixelHeight+1);
             CGContextSetRGBFillColor(context, pixR[x][y]/255.0, pixG[x][y]/255.0, pixB[x][y]/255.0, 1.0);
-            CGContextSetRGBStrokeColor(context, 1.0, 0.0, 0.0, 1.0);
+            CGContextSetRGBStrokeColor(context, pixR[x][y]/255.0, pixG[x][y]/255.0, pixB[x][y]/255.0, 1.0);
             CGContextFillRect(context, rectangle);
         }
     }
