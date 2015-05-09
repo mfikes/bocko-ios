8d6e4641d12dd8bdb48565c4a5ff51ea3e5cd1a7
Mike Fikes <mike@fikesfarm.com>
2015-05-09 09:50:30 -0400

    Initial copies of iOS and ClojureScript projects

---
+++ ClojureScript/project.clj
@@ -0,0 +1,20 @@
+(defproject bocko-ios "0.1.0-SNAPSHOT"
+  :description "Use Bocko on iOS"
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+                 [org.clojure/clojurescript "0.0-3255"]
+                 [org.omcljs/ambly "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.5"]]
+  :source-paths ["src"]
+  :clean-targets ["target" "out"]
+  :cljsbuild {:builds {:dev
+                       {:source-paths ["src"]
+                        :compiler     {:output-to      "target/out/main.js"
+                                       :output-dir     "target/out"
+                                       :optimizations  :none}}
+                       :rel
+                       {:source-paths ["src"]
+                        :compiler     {:output-to     "target/out/main.js"
+                                       :optimizations :advanced
+                                       :externs       ["externs.js"]
+                                       :pretty-print  false
+                                       :pseudo-names  false}}}})
---
+++ ClojureScript/src/bocko-ios/core.cljs
@@ -0,0 +1,1 @@
+(ns bocko-ios.core)
