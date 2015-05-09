24e05b5eb9705392379bdb6a5e7966bfae8b0a3f
Mike Fikes <mike@fikesfarm.com>
2015-05-09 11:03:46 -0400

    Stub out raster monitor

--- ClojureScript/src/bocko_ios/core.cljs
+++ ClojureScript/src/bocko_ios/core.cljs
@@ -1,2 +1,12 @@
 (ns bocko-ios.core
   (:require bocko.core))
+
+(defn init
+  []
+  (bocko.core/set-create-canvas
+    (fn [color-map raster width height pixel-width pixel-height]
+      (println "creating canvas" color-map)
+      (add-watch raster :monitor
+        (fn [_ _ o n]
+          (when (not= o n)
+            (prn n)))))))
