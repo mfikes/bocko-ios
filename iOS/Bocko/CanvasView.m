d2ee232d44c89efc3f742919aa64f5a12c75325e
Mike Fikes <mike@fikesfarm.com>
2015-05-11 17:22:01 -0400

    Center display

--- iOS/Bocko/CanvasView.m
+++ iOS/Bocko/CanvasView.m
@@ -11,8 +11,13 @@ static int bpix[40][40];
     CGSize size = self.frame.size;
     int pixelWidth = (int)size.width/40.0;
     int pixelHeight = (int)size.height/40.0;
+    int xOffset = (int)((size.width - 40*pixelWidth)/2);
+    int yOffset = (int)((size.height - 40*pixelHeight)/2);
     
-    CGRect rectangle = CGRectMake(x*pixelWidth, y*pixelHeight, pixelWidth, pixelHeight);
+    CGRect rectangle = CGRectMake(xOffset + x*pixelWidth,
+                                  yOffset + y*pixelHeight,
+                                  pixelWidth,
+                                  pixelHeight);
     
     rpix[x][y] = r;
     gpix[x][y] = g;
@@ -26,9 +31,13 @@ static int bpix[40][40];
     CGSize size = self.frame.size;
     int pixelWidth = (int)size.width/40.0;
     int pixelHeight = (int)size.height/40.0;
+    int xOffset = (int)((size.width - 40*pixelWidth)/2);
+    int yOffset = (int)((size.height - 40*pixelHeight)/2);
     
-    int x0 = ((int)(rect.origin.x))/pixelWidth;
-    int y0 = ((int)(rect.origin.y))/pixelHeight;
+    int x0 = ((int)(rect.origin.x)-xOffset)/pixelWidth;
+    int y0 = ((int)(rect.origin.y)-yOffset)/pixelHeight;
+    
+
     int xT = x0 + rect.size.width/pixelWidth;
     int yT = y0 + rect.size.height/pixelHeight;
     
@@ -36,15 +45,21 @@ static int bpix[40][40];
     
     for (int x=x0; x<xT; x++) {
         for (int y=y0; y<yT; y++) {
-            CGRect pixRect = CGRectMake(x*pixelWidth, y*pixelHeight, pixelWidth, pixelHeight);
+            CGRect pixRect = CGRectMake(xOffset + x*pixelWidth,
+                                        yOffset + y*pixelHeight,
+                                        pixelWidth,
+                                        pixelHeight);
+            
             int r = rpix[x][y];
             int g = gpix[x][y];
             int b = bpix[x][y];
+            
             CGContextSetRGBFillColor(context, r/255.0, g/255.0, b/255.0, 1.0);
             CGContextSetRGBStrokeColor(context, r/255.0, g/255.0, b/255.0, 1.0);
             CGContextFillRect(context, pixRect);
         }
     }
+    
     
 }
 
