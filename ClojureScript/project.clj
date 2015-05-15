5b2b1e1fdf6f6984a5e5227e752ce25b4f5104bb
Mike Fikes <mike@fikesfarm.com>
2015-05-15 09:22:16 -0400

    Update to Clojure 1.7.0-beta3

--- ClojureScript/project.clj
+++ ClojureScript/project.clj
@@ -1,6 +1,6 @@
 (defproject bocko-ios "0.2.0"
   :description "Use Bocko on iOS"
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
                  [org.clojure/clojurescript "0.0-3269"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [bocko "0.3.0"]
