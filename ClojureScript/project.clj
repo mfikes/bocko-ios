03e135eeddffc93af0a3685c41c4f77a89cb1d64
Mike Fikes <mike@fikesfarm.com>
2016-11-12 20:41:02 -0500

    1.0.0

--- ClojureScript/project.clj
+++ ClojureScript/project.clj
@@ -1,11 +1,11 @@
-(defproject bocko-ios "0.2.0"
+(defproject bocko-ios "1.0.0"
   :description "Use Bocko on iOS"
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
-                 [org.clojure/clojurescript "0.0-3269"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [bocko "0.3.0"]
-                 [org.omcljs/ambly "0.3.0"]]
-  :plugins [[lein-cljsbuild "1.0.5"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.293"]
+                 [org.clojure/core.async "0.2.395"]
+                 [bocko "1.0.0"]
+                 [org.omcljs/ambly "0.7.0"]]
+  :plugins [[lein-cljsbuild "1.1.4"]]
   :source-paths ["src"]
   :clean-targets ["target" "out"]
   :cljsbuild {:builds {:dev
